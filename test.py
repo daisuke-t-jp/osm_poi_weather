652dee117d8147a170fea3566b0371b6f9951a93
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2020-03-08 03:20:11 +0900

    Update test.py

--- test.py
+++ test.py
@@ -21,60 +21,68 @@ OPENWEATHERMAP_API_KEY = '<YOU MUST BE SET>'
 # - - - - - - - - - - - - - - - - - - - -
 def test_overpass_api():
     weathers = overpass_weahter.weathers_with_overpass_api("""
-                                        [out:json];
-                                        area["name"~"日本"];
-                                        node(area)["building"="church"];
-                                        out body;
-                                        """,
-                                        OPENWEATHERMAP_API_KEY,
-                                        0.1)
-
-    '''
-    overpass_weahter.weathers_with_overpass_api("""
-                                        [out:json];
-                                        area["name"~"日本"];
-                                        node(area)["amenity"="townhall"];
-                                        out body;
-                                        """,
-                                        OPENWEATHERMAP_API_KEY,
-                                        0.1)
-    '''
-
-    '''
-    overpass_weahter.weathers_with_overpass_api("""
-                                        [out:json];
-                                        area["name"~"日本"];
-                                        node(area)["public_transport"="station"];
-                                        out body;
-                                        """,
-                                        OPENWEATHERMAP_API_KEY,
-                                        0.1)
-    '''
-
-    for weather in weathers:
-        logging.debug('name[{0}] lat[{1}] lon[{2}] temp[{3}] pressure[{4}] humidity[{5}]'.format(
-            weather[overpass_weahter.KEY_NAME],
-            weather[overpass_weahter.KEY_LAT],
-            weather[overpass_weahter.KEY_LON],
-            weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['temp'],
-            weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['pressure'],
-            weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['humidity']))
+    try:
+        weathers = overpass_weahter.weathers_with_overpass_api("""
+                                            [out:json];
+                                            area["name"~"日本"];
+                                            node(area)["building"="church"];
+                                            out body;
+                                            """,
+                                            OPENWEATHERMAP_API_KEY,
+                                            0.1)
+    
+        '''
+        overpass_weahter.weathers_with_overpass_api("""
+                                            [out:json];
+                                            area["name"~"日本"];
+                                            node(area)["amenity"="townhall"];
+                                            out body;
+                                            """,
+                                            OPENWEATHERMAP_API_KEY,
+                                            0.1)
+        '''
+    
+        '''
+        overpass_weahter.weathers_with_overpass_api("""
+                                            [out:json];
+                                            area["name"~"日本"];
+                                            node(area)["public_transport"="station"];
+                                            out body;
+                                            """,
+                                            OPENWEATHERMAP_API_KEY,
+                                            0.1)
+        '''
+        for weather in weathers:
+            logging.debug('name[{0}] lat[{1}] lon[{2}] temp[{3}] pressure[{4}] humidity[{5}]'.format(
+                weather[overpass_weahter.KEY_NAME],
+                weather[overpass_weahter.KEY_LAT],
+                weather[overpass_weahter.KEY_LON],
+                weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['temp'],
+                weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['pressure'],
+                weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['humidity']))
             
+    except Exception as exp: 
+        logging.error('exception {0}'.format(exp))
+
     return
 
 
 def test_overpass_file():
-    weathers = overpass_weahter.weathers_with_overpass_file('overpass_japan_building_church.json', OPENWEATHERMAP_API_KEY, 0.1)
+    try:
+        weathers = overpass_weahter.weathers_with_overpass_file('voverpass_japan_building_church.json', OPENWEATHERMAP_API_KEY, 0.1)
+
+        for weather in weathers:
+            logging.debug('name[{0}] lat[{1}] lon[{2}] temp[{3}] pressure[{4}] humidity[{5}]'.format(
+                weather[overpass_weahter.KEY_NAME],
+                weather[overpass_weahter.KEY_LAT],
+                weather[overpass_weahter.KEY_LON],
+                weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['temp'],
+                weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['pressure'],
+                weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['humidity']))
+    except Exception as exp: 
+        logging.error('exception {0}'.format(exp))
     # weathers = overpass_weahter.weathers_with_overpass_file('overpass_japan_amenity_townhall.json', OPENWEATHERMAP_API_KEY, 0.1)
     # weathers = overpass_weahter.weathers_with_overpass_file('overpass_japan_public_transport_station.json', OPENWEATHERMAP_API_KEY, 0.1)
-    for weather in weathers:
-        logging.debug('name[{0}] lat[{1}] lon[{2}] temp[{3}] pressure[{4}] humidity[{5}]'.format(
-            weather[overpass_weahter.KEY_NAME],
-            weather[overpass_weahter.KEY_LAT],
-            weather[overpass_weahter.KEY_LON],
-            weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['temp'],
-            weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['pressure'],
-            weather[overpass_weahter.KEY_OPENWEATHERMAP_CURRENT_WEATHER_DATA]['main']['humidity']))
         
     return
 
