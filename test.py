b45ca9a351f8ecd0130219530c3de7dc61760ab1
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2020-03-07 09:58:43 +0900

    Update test.py

--- test.py
+++ test.py
@@ -17,12 +17,45 @@ OPENWEATHERMAP_API_KEY = '<YOU MUST BE SET>'
 
 
 # - - - - - - - - - - - - - - - - - - - -
-# Functions - Main
+# Functions
 # - - - - - - - - - - - - - - - - - - - -
+def test_local():
+    overpass_weahter.weathers_from_local_overpass_file(OPENWEATHERMAP_API_KEY, 'overpass_master_building_church.json')
+    # overpass_weahter.weathers_from_local_overpass_file(OPENWEATHERMAP_API_KEY, 'overpass_master_amenity_townhall.json')
+    # overpass_weahter.weathers_from_local_overpass_file(OPENWEATHERMAP_API_KEY, 'overpass_master_public_transport_station.json')
+    return
+
+
+def test_server():
+    overpass_weahter.weathers_from_server(OPENWEATHERMAP_API_KEY, """
+                                        [out:json];
+                                        area["name"~"日本"];
+                                        node(area)["building"="church"];
+                                        out body;
+                                        """)
+    return
+'''
+    overpass_weahter.weathers_from_server(OPENWEATHERMAP_API_KEY, """
+                                        [out:json];
+                                        area["name"~"日本"];
+                                        node(area)["amenity"="townhall"];
+                                        out body;
+                                        """)
+
+    overpass_weahter.weathers_from_server(OPENWEATHERMAP_API_KEY, """
+                                        [out:json];
+                                        area["name"~"日本"];
+                                        node(area)["public_transport"="station"];
+                                        out body;
+                                        """)
+'''
+
+
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
     
-    overpass_weahter.weathers_from_local_overpass_file(OPENWEATHERMAP_API_KEY, 'overpass_master_building_church.json')
+    test_local()
+    test_server()
     
 if __name__ == '__main__':
     main()
